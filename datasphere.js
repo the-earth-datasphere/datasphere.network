b0fd0c19d1dce6ff16be583a83ffaf9cefe0a68f
Andrei Mititelu <andreimititelu@outlook.com>
2020-04-24 21:26:58 +0300

    Load modules

--- datasphere.js
+++ datasphere.js
@@ -1,3 +1,19 @@
+/* =====================================================================
+    Modules
+=======================================================================*/
+
+// Logs
 const Log = require('./lib/logger');
 
 Log.info('Datasphere is alive!');
+
+// Env variables
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env'});
+
+// Express
+const express = require('express');
+const app = express();
+// Body Parser
+app.use(express.json());
+
