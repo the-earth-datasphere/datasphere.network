6da58d82314f4b2f3941ab302da20afcce1e5036
Andrei Mititelu <andreimititelu@outlook.com>
2020-04-25 00:38:10 +0300

    MQ Broker class

--- datasphere.js
+++ datasphere.js
@@ -41,4 +41,3 @@ process.on('unhandledRejection', (err, promise) => {
     // Close server and exit process
     server.close(() => process.exit(1));
 });
-
---
+++ server/broker.js
@@ -0,0 +1,73 @@
+const amqp = require('amqplib/callback_api');
+const Log = require('../lib/logger');
+
+class Broker {
+
+    constructor (host) {
+        this.host = host;
+    }
+
+    pull (queue) {
+        Log.info(`p2p.message.broker.pull ${queue}`);
+        amqp.connect(this.host, (error0, connection) => {
+            if (error0) {
+                Log.error(`p2p.message.broker.pull.connect.error ${error0}`);
+                throw error0;
+            }
+            connection.createChannel((error1, channel) => {
+                if (error1) {
+                    Log.error(`p2p.message.broker.pull.connect.channel.error ${error1}`);
+                    throw error1;
+                }
+
+                channel.assertQueue(queue, {
+                    durable: false
+                });
+
+                Log.info(`p2p.message.broker.pull.connect.channel.listen.queue ${queue}`);
+
+                channel.consume(queue, (message) => {
+                    Log.info(`p2p.message.broker.pull.connect.channel.consume.queue ${queue}`);
+                    Log.info(`p2p.message.broker.pull.connect.channel.consume.queue.message ${message.content.toString()}`);
+                }, {
+                    noAck: true
+                });
+            });
+        });
+
+    }
+
+    push (message, queue) {
+
+        Log.info(`message.broker.push ${queue}`);
+        amqp.connect(this.host, (error0, connection) => {
+            if (error0) {
+                Log.error(`p2p.message.broker.push.connect.error ${error0}`);
+                throw error0;
+            }
+            connection.createChannel((error1, channel) => {
+                if (error1) {
+                    Log.error(`p2p.message.broker.push.connect.channel.error ${error1}`);
+                    throw error1;
+                }
+
+                channel.assertQueue(queue, {
+                    durable: false
+                });
+                
+                channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+                Log.info(`p2p.message.broker.push.connect.channel.send.queue ${queue}`);
+                Log.info(`p2p.message.broker.push.connect.channel.send.queue.message ${message}`);
+            });
+
+            setTimeout(async () => {
+                await connection.close();
+                // process.exit(0);
+            }, 1000);
+        });
+    }
+
+
+}
+
+module.exports = Broker;
