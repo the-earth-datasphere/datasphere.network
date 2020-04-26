15497c7ce07eecc6918a5b5fcec6a45ca1fa1127
Andrei Mititelu <andreimititelu@outlook.com>
2020-04-26 14:48:22 +0300

    test network load

--- controllers/index.js
+++ controllers/index.js
@@ -32,3 +32,37 @@ exports.postDataToSphere = async (req, res, next) => {
         }); 
     }
 }
+
+// @desc    Post Test Load Data to Sphere
+// @route   POST api/v1/load
+// @access  Public
+
+exports.postLoadTestDataToSphere = async (req, res, next) => {
+    try {
+        Log.info(`app.server.post.load.test.data.request ${req.method} ${req.originalUrl}`);
+
+        const messagesNo = 5; // how many messages to send per request, use with care on the same machine
+
+        const params = {
+            type : 'standard',
+            reqHeaders: false 
+        };
+
+        const dataObject = Data.buildDataObject(req, params);
+
+        for (let i=0; i<messagesNo; i++) {
+            dm.storeData(dataObject);
+            dm.broadcastData(dataObject);
+        }
+
+        Log.info(`app.server.post.load.test.data.response ${req.method} ${req.originalUrl} 200`);
+
+        res.status(200).json({success: true});
+    } catch (err) {
+        Log.error(`app.server.post.data.response ${req.method} ${req.originalUrl} 400 ${err}`);
+        res.status(400).json({
+            success: false, 
+            error: err
+        }); 
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
 const {
-    postDataToSphere
+    postDataToSphere,
+    postLoadTestDataToSphere
 } = require('../controllers');
 
 const router = express.Router();
 
 router.route('/datasphere')
             .post(postDataToSphere);
+
+router.route('/load')
+            .post(postLoadTestDataToSphere);
 
 module.exports = router;
